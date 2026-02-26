3a4efea3ef8c311e3f0d0efc08ad6b78b52da5a4
tubbairfan <tubbairfan14@gmail.com>
2026-02-26 11:04:16 +0500

    regenrate prisma1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,5 +4,4 @@ import { PrismaClient } from "@prisma/client";
 const connectionString = `${process.env.DATABASE_URL}`
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
-
 export { prisma }
